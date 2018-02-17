367cf1be6510c88291c834616ad31deea83120b1
Matt English <menglish2451@gmail.com>
2018-02-16 22:17:20 -0800

    Try out using Bulma

--- app/components/army-builder-home.vue
+++ app/components/army-builder-home.vue
@@ -1,49 +1,67 @@
 <template>
-  <div>
+  <div class="container">
     <section class="section">
       <h1 class="title">Army Builder</h1>
-      <form>
-        <div class="field">
-          <label class="label">Pick Nation</label>
-          <div class="control">
-            <div class="select">
-              <select>
-                <option>Germany</option>
-              <select>
+      <div class="columns">
+        <div class="column">
+          <form>
+            <div class="field">
+              <label class="label">Pick Nation</label>
+              <div class="control">
+                <div class="select">
+                  <select>
+                    <option>Germany</option>
+                  <select>
+                </div>
+              </div>
             </div>
-          </div>
+            <div>
+              <div>
+                <label class="checkbox" disabled>
+                  <input type="checkbox" disabled>
+                  Use theater selection
+                </label>
+              </div>
+              <!-- First select unit type -->
+              <div>
+                <label class="label">Officer</label>
+                <div class="control">
+                  <div class="select">
+                    <select>
+                      <option></option>
+                      <option>Second Lieutenant</option>
+                      <option>First Lieutenant</option>
+                    <select>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <!-- Second select unit exp level -->
+                <!-- This selection will depend on what force was selected -->
+                <label class="label">Exp</label>
+                <div class="control">
+                  <div class="select">
+                    <select>
+                      <option></option>
+                      <option>Regular - 50pts</option>
+                      <option>Experienced - 65pts</option>
+                    <select>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <!--  Options for each unit - extra men or weapons -->
+                <label>Extra Men</label>
+                <select>
+                  <option>0</option>
+                  <option>1</option>
+                  <option>2</option>
+                <select>
+              </div>
+            </div>
+          </form>
         </div>
-        <div>
-          <label>Use theater selection</label>
-          <input type="checkbox"/>
-        </div>
-        <div>
-          <div>
-            <label>Officer</label>
-            <select>
-              <option></option>
-              <option>Second Lt. - 40pts</option>
-              <option>First Lt. - 50pts</option>
-            <select>
-          </div>
-          <div>
-            <label>Exp.</label>
-            <select>
-              <option></option>
-              <option>Regular - 40pts</option>
-              <option>Experienced - 50pts</option>
-            <select>
-          </div>
-          <div>
-            <label>Extra Men</label>
-            <select>
-              <option>0</option>
-              <option>1</option>
-              <option>2</option>
-            <select>
-          </div>
-        </div>
-      </form>
+      </div>
     </section>
   </div>
 </template>
