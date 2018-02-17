c438ab571b156ae721c248dce34b52a6a9ca72c7
Matt English <menglish2451@gmail.com>
2018-02-16 18:39:44 -0800

    Setup and integrate bulma

--- app/components/army-builder-home.vue
+++ app/components/army-builder-home.vue
@@ -1,18 +1,50 @@
 <template>
   <div>
-    <h1>Army Builder</h1>
-    <form>
-      <div>
-        <label>Nationality</label>
-        <select>
-          <option>Germany</option>
-        <select>
-      </div>
-      <div>
-        <label>Use theater selection</label>
-        <input type="checkbox"/>
-      </div>
-    </form>
+    <section class="section">
+      <h1 class="title">Army Builder</h1>
+      <form>
+        <div class="field">
+          <label class="label">Pick Nation</label>
+          <div class="control">
+            <div class="select">
+              <select>
+                <option>Germany</option>
+              <select>
+            </div>
+          </div>
+        </div>
+        <div>
+          <label>Use theater selection</label>
+          <input type="checkbox"/>
+        </div>
+        <div>
+          <div>
+            <label>Officer</label>
+            <select>
+              <option></option>
+              <option>Second Lt. - 40pts</option>
+              <option>First Lt. - 50pts</option>
+            <select>
+          </div>
+          <div>
+            <label>Exp.</label>
+            <select>
+              <option></option>
+              <option>Regular - 40pts</option>
+              <option>Experienced - 50pts</option>
+            <select>
+          </div>
+          <div>
+            <label>Extra Men</label>
+            <select>
+              <option>0</option>
+              <option>1</option>
+              <option>2</option>
+            <select>
+          </div>
+        </div>
+      </form>
+    </section>
   </div>
 </template>
 
