e0581ce27f5b05e84f6a8073608b19bb3627f21f
Matt English <menglish2451@gmail.com>
2018-02-17 21:35:52 -0800

    Create insert statement

--- app/models/army-builder-data.js
+++ app/models/army-builder-data.js
@@ -1,6 +1,27 @@
 const mongodb = require('mongodb');
 const uri = process.env.MONGODB_URI;
+const dbName = process.env.DB_NAME;
 
-mongodb.MongoClient.connect(uri, function(err, client) {
+const seedData = [
+    {
+        type: 'HQ unit',
+        name: 'Officer',
+        cost: [ {name: 'Second Lieutentant', regular: 50, veteran: 65}, 
+                {name: 'First Lieutentant', regular: 75, veteran: 90} ],
+        team: '1 officer and up to 2 additional men',
+        weapons: [ 'pistol', 'submachine gun', 'rifle', 'assault rifle' ],
+        options: [ {name: 'Bodyguard', regular: 10, veteran: 13} ]
+    }
+];
+
+mongodb.MongoClient.connect(uri, (err, client) => {
     if (err) throw err;
+
+    let db = client.db(dbName);
+    let army = db.collection('army');
+
+    // TODO setup migration for db
+    // army.insert(seedData, (err, result) => {
+    //     if (err) throw err;
+    // });
 });
