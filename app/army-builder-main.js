8562a5a5c939acc7715b3cdebf5408a6d30f1a5f
Matt English <menglish2451@gmail.com>
2018-02-18 19:42:28 -0800

    Add vuex

--- app/army-builder-main.js
+++ app/army-builder-main.js
@@ -1,11 +1,23 @@
 // main.js
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './army-builder-app.vue'
 import { createRouter } from './army-builder-router.js'
 
 // export a factory function for creating fresh app, router and store
 // instances
 export function createApp() {
+  Vue.use(Vuex)
+  const store = new Vuex.Store({
+    state: {
+      count: 0
+    },
+    mutations: {
+      increment (state) {
+        state.count++
+      }
+    }
+  });
   // create router instance
   const router = createRouter();
 
